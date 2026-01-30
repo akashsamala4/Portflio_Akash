478f57b8a58019fba92f9be178b547815630165b
akashsamala4 <samalaakash4@gmail.com>
2026-01-30 14:49:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// Show welcome message
-function showMessage() {
-  alert("Welcome to Akash's Portfolio Website!");
-}
+emailjs.init("FcxhVsLJdCnsJKQaV");
 
-// Toggle dark mode
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-}
+function sendMail() {
+  const params = {
+    name: name.value,
+    email: email.value,
+    message: message.value
+  };
 
-// Form validation
-function validateForm(event) {
-  event.preventDefault();
-
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
-
-  if (email === "" || !email.includes("@")) {
-    alert("Please enter a valid email");
-    return false;
-  }
-
-  if (message.length < 5) {
-    alert("Message must be at least 5 characters");
-    return false;
-  }
-
-  alert("Form submitted successfully!");
-  return true;
+  emailjs.send("service_g9phtli", "template_4uer9yr", params)
+    .then(() => status.innerText = "Message sent successfully!")
+    .catch(() => status.innerText = "Failed to send message!");
 }
